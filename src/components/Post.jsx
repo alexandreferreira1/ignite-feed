883f784eda5eb81afcd4fc3c16ea46a45cfae71b
alexandreximenez <alexandre.x.f@hotmail.com>
2024-07-09 23:42:49 -0300

    Finish Structural and Visual

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,5 @@
+import { Avatar } from "./Avatar";
+import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
 export function Post() {
@@ -5,7 +7,7 @@ export function Post() {
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img className={styles.avatar} src="https://github.com/diego3g.png" />
+          <Avatar src="https://github.com/diego3g.png" />
           <div className={styles.authorInfo}>
             <strong>Diego Fernandes</strong>
             <span>Web Developer</span>
@@ -26,14 +28,29 @@ export function Post() {
         </p>
 
         <p>
-          👉 {' '} <a>jane.design/doctorcare</a>
+          👉 <a>jane.design/doctorcare</a>
         </p>
 
         <p>
-          <a href="">#novoprojeto</a>{' '}
-          <a href="">#nlw</a>{' '}
+          <a href="">#novoprojeto</a> <a href="">#nlw</a>{" "}
           <a href="">#rocketseat</a>
         </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea placeholder="Deixe um comentário" />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment/>
       </div>
     </article>
   );
